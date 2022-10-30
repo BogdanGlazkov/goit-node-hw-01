d49ce9b911698e2ca3e368a629d9d236fc489276
BogdanGlazkov <bglazkov@i.ua>
2022-10-30 15:35:39 +0200

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const result = await fs.readFile(contactsPath);
+    return JSON.parse(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const contact = JSON.parse(contacts).filter(
+      (item) => +item.id === contactId
+    );
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const parsedContacts = JSON.parse(contacts);
+    const restContacts = parsedContacts.filter(
+      (item) => +item.id !== contactId
+    );
+    fs.writeFile(contactsPath, JSON.stringify(restContacts));
+    return restContacts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const parsedContacts = JSON.parse(contacts);
+    const id = parsedContacts.length + 1;
+    const newContact = {
+      id: id.toString(),
+      name,
+      email,
+      phone,
+    };
+    const updatedContactList = [...parsedContacts, newContact];
+    const strList = JSON.stringify(updatedContactList);
+    fs.writeFile(contactsPath, strList);
+    return updatedContactList;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
